b70027e55f8fbaf467dd5a0c9a1577bac63dca2d
Aaron-KASONGO <19kk181@esisalama.org>
2021-04-25 19:14:08 +0100

    Configuration for add a Task

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'my_app/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'my_app/index.html', {'tasks': tasks})
+
+
+def add_task(request):
+    Task.objects.create(title=request.POST['content'])
+    return redirect('index')
